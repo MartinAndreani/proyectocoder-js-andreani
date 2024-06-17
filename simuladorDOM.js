b7f162e878dbeb8f726c8d68c9b0abb8ead9467b
Martin <andreani.martin4@gmail.com>
2024-06-17 17:21:55 -0300

    commit

--- simuladorDOM.js
+++ simuladorDOM.js
@@ -1,10 +1,4 @@
-if (typeof localStorage === "undefined" || localStorage === null) {
-    var LocalStorage = require('node-localstorage').LocalStorage;
-    localStorage = new LocalStorage('./scratch');
-  }
-  
-  localStorage.setItem('myFirstKey', 'myFirstValue');
-  console.log(localStorage.getItem('myFirstKey'));
+
 
 // CREAR CLASE
 class Cancion{
